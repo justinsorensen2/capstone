97c50196f2924b5011ff3ad92fbf94a7a608c37e
justinsorensen2dotnet <>
2020-04-09 14:09:21 -0400

    got JWT_KEY working

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,6 +42,10 @@ namespace capstone
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      foreach (var item in Configuration.AsEnumerable())
+      {
+        Console.WriteLine($"{ item.Key} = ${ item.Value}");
+      }
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
